3161673527dbe5269922dcc4dc51546cf49019a5
Dimo Mitev <dimo.mitev@telerik.com>
2018-09-20 11:10:31 +0300

    Add more files

---
+++ flex-automation-article/src/index.js
@@ -0,0 +1,16 @@
+"use-strict"
+
+const kinveyFlexSDK = require('kinvey-flex-sdk');
+
+kinveyFlexSDK.service((err, flex) => {
+    if (err) {
+       console.log("Error while initializing Flex!");
+       return; 
+    }
+
+    flex.functions.register("testFunction", (context, complete, modules) => {
+        flex.logger.info("I am a simple test function.");
+        flex.logger.warn("I will do nothing special.");
+        return complete().setBody( { success: "true", serviceVersion: "1.0.0"} ).ok().next();
+    });
+});
