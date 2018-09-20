d026863bdbd303189bd1bc1fbce51f02a88b1e4c
Dimo Mitev <dimo.mitev@telerik.com>
2018-09-20 13:57:50 +0300

    Bump version

--- flex-automation-article/src/index.js
+++ flex-automation-article/src/index.js
@@ -4,13 +4,13 @@ const kinveyFlexSDK = require('kinvey-flex-sdk');
 
 kinveyFlexSDK.service((err, flex) => {
     if (err) {
-       console.log("Error while initializing Flex!");
-       return; 
+        console.log("Error while initializing Flex!");
+        return;
     }
 
     flex.functions.register("testFunction", (context, complete, modules) => {
         flex.logger.info("I am a simple test function.");
         flex.logger.warn("I will do nothing special.");
-        return complete().setBody( { success: "true", serviceVersion: "2.0.0"} ).ok().next();
+        return complete().setBody({ success: "true", serviceVersion: "3.0.0" }).ok().next();
     });
 });
